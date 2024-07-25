a99b38231d35560252442f5bbc4b18b8e689cd2d
shwncho <shwncho4@gmail.com>
2024-07-25 10:53:52 +0900

    Loop

---
+++ src/main/kotlin/loop/ForSample.kt
@@ -0,0 +1,16 @@
+package loop
+class ForSample {
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      for (i in 1..5) println(i)
+      println()
+
+      for (i in 5 downTo 1) println(i)
+      println()
+
+      for (i in 1..5 step 2) println(i)
+      println()
+    }
+  }
+}
